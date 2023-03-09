23226549c0453d7ebe0b8904467e4719e457c59d
Danjuma Nasiru <talk2danjumanas@gmail.com>
2023-03-09 17:01:27 +0100

    Initial Commit

---
+++ FlashZillaApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlashZillaApp
+//
+//  Created by Danjuma Nasiru on 09/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlashZillaApp/FlashZillaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaAppApp.swift
+//  FlashZillaApp
+//
+//  Created by Danjuma Nasiru on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
